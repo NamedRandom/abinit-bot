0d79d54710de7e263d54db5004316e6dcf2064aa
Your Name <you@example.com>
2019-07-11 13:43:37 -0400

    goto slexy.org because raw paste

--- abinitMulti.sh
+++ abinitMulti.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-~/gdown.pl $1 out.xz > /dev/null 2>&1
+scriptDir=`pwd`
+dlDir=downloads
+cores=4
+mkdir $dlDir  > /dev/null 2>&1
+cd $dlDir
+$scriptDir/gdown.pl $1 out.xz > /dev/null 2>&1
 unxz out.xz > /dev/null 2>&1
-tar xf out > /dev/null 2>&1
+tar xf out > /dev/null 2>&1     
 dir=`tar tf out | head -1 | cut -f1 -d"/"`
+rm -rf out out.xz
 cd "$dir"
-mpirun -n 7 abinit < tbase*_x.files > log 2> err
-echo "Result at: "`pastebinit tbase*_*.out 2> /dev/null`
-echo "If there was an error it's here: "`pastebinit err 2> /dev/null`
-cd ..
-rm -rf "$dir" out out.xz
+mpirun -n $cores abinit --verbose < tbase*_x.files > log 2> err
+if [ "$(wc -l err)" != "0 err" ]
+then
+	echo Error: `pastebinit -b slexy.org err 2> /dev/null`
+fi
+echo Results: `pastebinit -b slexy.org tbase*_*.out 2> /dev/null`
