d79e0a1885a583b1ad57dfb40730e55a54c48bf4
Your Name <you@example.com>
2019-07-12 14:41:55 -0400

    change multi to suppress mpi warns to err, now not posting err if there isnt one

--- abinitMulti.sh
+++ abinitMulti.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 scriptDir=`pwd`
 dlDir=downloads
-cores=4
+cores=12
 mkdir $dlDir  > /dev/null 2>&1
 cd $dlDir
 $scriptDir/gdown.pl $1 out.xz > /dev/null 2>&1
@@ -10,7 +10,7 @@ tar xf out > /dev/null 2>&1
 dir=`tar tf out | head -1 | cut -f1 -d"/"`
 rm -rf out out.xz
 cd "$dir"
-mpirun -n $cores abinit --verbose < tbase*_x.files > log 2> err
+mpirun -n $cores -q abinit --verbose < tbase*_x.files > log 2> err
 if [ "$(wc -l err)" != "0 err" ]
 then
 	echo Error: `pastebinit -b slexy.org err 2> /dev/null`
